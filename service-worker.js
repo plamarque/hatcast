583d8a7cf026629aabb56b74bf3d1e6efbeb9b5b
Patrice Lamarque <patrice.lamarque@gmail.com>
2025-08-12 13:14:54 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1224,7 +1224,7 @@ function re(a, e) {
   ne(a), ee(e);
 }
 self.__WB_DISABLE_DEV_LOGS = !0;
-re([{"revision":"f64ff44dbdd343b467450754f752d0b1","url":"__/auth/action.html"},{"revision":"e01ba3cee9fa09d1c4d5e7404a20456e","url":"404.html"},{"revision":null,"url":"assets/index-74311357.js"},{"revision":null,"url":"assets/index-e03aff92.css"},{"revision":null,"url":"assets/index.esm-93305b69.js"},{"revision":null,"url":"assets/index.esm-dfc6116a.js"},{"revision":null,"url":"assets/workbox-window.prod.es5-5ffdab76.js"},{"revision":"30a9dea5ac265d2fae81dbe49dfa4206","url":"index.html"},{"revision":"395314d1c59952fccc31953ce10bdb0c","url":"icons/manifest-icon-192.maskable.png"},{"revision":"9ced2b98eac0ebef14fa51d0833de033","url":"icons/manifest-icon-512.maskable.png"},{"revision":"c60d87ae03ae87c7e7c056b94b95eeda","url":"manifest.webmanifest"}] || []);
+re([{"revision":"f64ff44dbdd343b467450754f752d0b1","url":"__/auth/action.html"},{"revision":"e01ba3cee9fa09d1c4d5e7404a20456e","url":"404.html"},{"revision":null,"url":"assets/index-352fcfe1.js"},{"revision":null,"url":"assets/index-e03aff92.css"},{"revision":null,"url":"assets/index.esm-0986cedc.js"},{"revision":null,"url":"assets/index.esm-e6873890.js"},{"revision":null,"url":"assets/workbox-window.prod.es5-5ffdab76.js"},{"revision":"a8316de93aad6674624a9c2a59e16436","url":"index.html"},{"revision":"395314d1c59952fccc31953ce10bdb0c","url":"icons/manifest-icon-192.maskable.png"},{"revision":"9ced2b98eac0ebef14fa51d0833de033","url":"icons/manifest-icon-512.maskable.png"},{"revision":"c60d87ae03ae87c7e7c056b94b95eeda","url":"manifest.webmanifest"}] || []);
 ae();
 self.skipWaiting();
 self.addEventListener("activate", (a) => {
